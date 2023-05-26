ee172b6cbb7a8471ed7571171a38cbe206196670
angelaldrete <angel.aldrete96@gmail.com>
2023-05-26 14:07:35 -0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+app.use(cors(
+  {
+    origin: 'https://theknobstudio.vercel.app/',
+    optionsSuccessStatus: 200
+  }
+));
+
+app.use('/assets/img', express.static(path.join(__dirname, '../public/assets/img')));
+app.use('/assets/audio', express.static(path.join(__dirname, '../public/assets/audio')));
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+})
