6ae04d6f37621e23eba35f72180631705011bf67
angelaldrete <angel.aldrete96@gmail.com>
2023-05-26 14:34:54 -0700

    Changed routes and paths

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(cors(
+  {
+    origin: 'https://theknobstudio.vercel.app',
+    optionsSuccessStatus: 200
+  }
+));
+
+app.use('/assets/img', express.static(path.join(__dirname, '../public/assets/img')));
+app.use('/assets/audio', express.static(path.join(__dirname, '../public/assets/audio')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
